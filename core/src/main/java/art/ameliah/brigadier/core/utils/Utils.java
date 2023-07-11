4eb38eb0e7a5cd0745a56745666be78d3aead1e7
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-07-11 17:49:59 +0200

    utils

--- core/src/main/java/art/ameliah/brigadier/core/utils/Utils.java
+++ core/src/main/java/art/ameliah/brigadier/core/utils/Utils.java
@@ -1,5 +1,8 @@
 package art.ameliah.brigadier.core.utils;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Utils {
 
   public static boolean typeIsBool(Class<?> obj) {
@@ -16,6 +19,10 @@ public class Utils {
 
   public static boolean typeIsDouble(Class<?> obj) {
     return obj.equals(double.class) || obj.equals(Double.class);
+  }
+
+  public static String stackTraceToString(StackTraceElement[] stackTraceElements) {
+    return Arrays.stream(stackTraceElements).map(StackTraceElement::toString).collect(Collectors.joining("\n"));
   }
 
 }
